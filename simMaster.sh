9783fef646f745892ab9acb9f1ed37831d069295
moebaid <moe.obaid@gmail.com>
2022-08-19 01:23:14 +0000

    Edited simMaster

--- simMaster.sh
+++ simMaster.sh
@@ -14,9 +14,10 @@ read start_sim
 
 end_sim=$((start_sim + num_splits - 1))
 
+
 python3 splitmacros.py $organ $num_splits $totalevents $start_sim $end_sim 
 
 cd ${organ}
 for (( n=$start_sim; n<=$end_sim; n++ )); do
-    nohup docker run -i --rm -v $PWD:/APP opengatecollaboration/gate main_normalized_${n}.mac > ${organ}.out &
+    nohup docker run -i --rm -v $PWD:/APP opengatecollaboration/gate main_normalized_${n}.mac > ${organ}_${n}.out & 
 done
